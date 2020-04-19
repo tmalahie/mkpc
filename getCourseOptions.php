40e1e76a28a2b41531267b88f0d9fa4c1a3697ee
Timothé Malahieude <timothe.malahieude@ecl14.ec-lyon.fr>
2020-04-19 17:55:47 +0200

    More responsive default screen width

--- getCourseOptions.php
+++ getCourseOptions.php
@@ -2,5 +2,5 @@
 	quality: <?php echo (isset($_COOKIE['iQuality']) ? $_COOKIE['iQuality']:5); ?>,
 	music: <?php echo (isset($_COOKIE['bMusic']) ? $_COOKIE['bMusic']:0); ?>,
 	sfx: <?php echo (isset($_COOKIE['iSfx']) ? $_COOKIE['iSfx']:(isset($_COOKIE['bMusic']) ? $_COOKIE['bMusic']:0)); ?>,
-	screenscale: <?php echo (isset($_COOKIE['iScreenScale']) ? $_COOKIE['iScreenScale']:'(screen.width<480)?4:6'); ?>
+	screenscale: <?php echo (isset($_COOKIE['iScreenScale']) ? $_COOKIE['iScreenScale']:'(screen.width<800)?((screen.width<480)?4:6):((screen.width<1500)?8:10)'); ?>
 }
