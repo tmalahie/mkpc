114ca4ee77f9936daf7ed758c6bfd171518b438b
root <root@m50.evxonline.net>
2020-05-14 09:17:46 +0200

    Don't show birthdays for non-connected people

--- index.php
+++ index.php
@@ -559,44 +559,44 @@ $slidesPath = 'images/slides';
 		?>
 		<div class="subsection">
 		<?php
-		date_default_timezone_set(get_client_tz());
+		if ($id) {
+			date_default_timezone_set(get_client_tz());
 
-		$today = time();
-		$cYear = date('Y', $today);
-		$cMonth = date('m', $today);
-		$cDay = date('d', $today);
-		$curDate = $cYear.'-'.$cMonth.'-'.$cDay;
-		date_default_timezone_set('UTC');
-		$getBirthdays = mysql_query('SELECT j.id,j.nom,p.identifiant,p.identifiant2,p.identifiant3,p.identifiant4,p.nbmessages FROM `mkprofiles` p INNER JOIN `mkjoueurs` j ON p.id=j.id WHERE birthdate IS NOT NULL AND DAY(birthdate)='. $cDay .' AND MONTH(birthdate)='. $cMonth .' AND j.banned=0 AND j.deleted=0 AND last_connect>=DATE_SUB("'.$curDate.'",INTERVAL 1 YEAR) AND TIMESTAMPDIFF(SECOND,last_connect,"'.$curDate.'")<=TIMESTAMPDIFF(SECOND,IFNULL(sub_date,"2016-01-01"),last_connect)+7*24*3600 ORDER BY p.nbmessages DESC, p.id ASC');
-		$dc = array();
-		$birthdaysList = array();
-		while ($getBirthday = mysql_fetch_array($getBirthdays)) {
-			$dId = $getBirthday['identifiant'].'_'.$getBirthday['identifiant2'].'_'.$getBirthday['identifiant3'].'_'.$getBirthday['identifiant4'];
-			if (!isset($dc[$dId])) {
-				$dc[$dId] = $getBirthday;
-				$birthdaysList[] = $getBirthday;
+			$today = time();
+			$cYear = date('Y', $today);
+			$cMonth = date('m', $today);
+			$cDay = date('d', $today);
+			$curDate = $cYear.'-'.$cMonth.'-'.$cDay;
+			$getBirthdays = mysql_query('SELECT j.id,j.nom,p.identifiant,p.identifiant2,p.identifiant3,p.identifiant4,p.nbmessages FROM `mkprofiles` p INNER JOIN `mkjoueurs` j ON p.id=j.id WHERE birthdate IS NOT NULL AND DAY(birthdate)='. $cDay .' AND MONTH(birthdate)='. $cMonth .' AND j.banned=0 AND j.deleted=0 AND last_connect>=DATE_SUB("'.$curDate.'",INTERVAL 1 YEAR) AND TIMESTAMPDIFF(SECOND,last_connect,"'.$curDate.'")<=TIMESTAMPDIFF(SECOND,IFNULL(sub_date,"2016-01-01"),last_connect)+7*24*3600 ORDER BY p.nbmessages DESC, p.id ASC');
+			$dc = array();
+			$birthdaysList = array();
+			while ($getBirthday = mysql_fetch_array($getBirthdays)) {
+				$dId = $getBirthday['identifiant'].'_'.$getBirthday['identifiant2'].'_'.$getBirthday['identifiant3'].'_'.$getBirthday['identifiant4'];
+				if (!isset($dc[$dId])) {
+					$dc[$dId] = $getBirthday;
+					$birthdaysList[] = $getBirthday;
+				}
+			}
+			$nbBirthdays = count($birthdaysList);
+			if ($nbBirthdays) {
+				?>
+				<div class="birthdays-list">
+					<img src="images/ic_birthday.png" alt="birthday" />
+					<?php echo $language ? "It's the birthday of":"C'est l'anniversaire de"; ?>
+					<?php
+					for ($i=0;$i<$nbBirthdays;$i++) {
+						$birthday = $birthdaysList[$i];
+						if ($i)
+							echo ($i==$nbBirthdays-1) ? ($language ? " and ":" et "):", ";
+						echo '<a href="profil.php?id='. $birthday['id'] .'">'. $birthday['nom'] .'</a>';
+					}
+					echo ($language ? '!':'&nbsp;!');
+					?>
+				</div>
+				<?php
 			}
 		}
-		$nbBirthdays = count($birthdaysList);
-		if ($nbBirthdays) {
-			?>
-			<div class="birthdays-list">
-				<img src="images/ic_birthday.png" alt="birthday" />
-				<?php echo $language ? "It's the birthday of":"C'est l'anniversaire de"; ?>
-				<?php
-				for ($i=0;$i<$nbBirthdays;$i++) {
-					$birthday = $birthdaysList[$i];
-					if ($i)
-						echo ($i==$nbBirthdays-1) ? ($language ? " and ":" et "):", ";
-					echo '<a href="profil.php?id='. $birthday['id'] .'">'. $birthday['nom'] .'</a>';
-				}
-				echo ($language ? '!':'&nbsp;!');
-				?>
-			</div>
-			<?php
-		}
-		?>
-		<?php
+		date_default_timezone_set('UTC');
 		display_sidebar('Forum', 'forum.php');
 		?>
 			<h1><?php echo $language ? 'Last topics':'Derniers topics'; ?></h1>
