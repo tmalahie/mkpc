16dae638a766da7003b4acee65d8bfdc8935c2e8
Timothé Malahieude <timothe.malahieude@ecl14.ec-lyon.fr>
2020-06-05 23:32:25 +0200

    Clear cache

--- draw.php
+++ draw.php
@@ -34,7 +34,11 @@ if (isset($_GET['i'])) {
 		</script>
 		<script src="scripts/vanilla-picker.min.js"></script>
 		<script type="text/javascript" src="scripts/editor.js"></script>
+<<<<<<< HEAD
 		<script type="text/javascript" src="scripts/draw.js"></script>
+=======
+		<script type="text/javascript" src="scripts/draw"></script>
+>>>>>>> Clear cache
 	</head>
 	<body onkeydown="handleKeySortcuts(event)" onbeforeunload="return handlePageExit()" class="editor-body">
 		<div id="editor-wrapper" onmousemove="handleMove(event)" onclick="handleClick(event)">
