d284de563dc5e54e38cc7fd32f0607b0ccc63cdb
Timothé Malahieude <timothe.malahieude@ecl14.ec-lyon.fr>
2020-06-21 18:51:46 +0200

    v73

--- mk/main.php
+++ mk/main.php
@@ -2,5 +2,5 @@
 if (in_array($_SERVER['HTTP_HOST'], array('local-mkpc.malahieude.info','mkpc-translations.000webhostapp.com')))
 	echo '<script type="text/javascript" src="scripts/mk.js?reload=2"></script>';
 else
-	echo '<script type="text/javascript" src="scripts/mk.v72.js"></script>';
+	echo '<script type="text/javascript" src="scripts/mk.v73.js"></script>';
 ?>
