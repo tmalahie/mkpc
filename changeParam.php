f508d42c8a85ff458387c032302a83fb812fc6d6
Timothé Malahieude <timothe.malahieude@ecl14.ec-lyon.fr>
2020-08-04 23:00:38 +0200

    Disable FPS feature

--- changeParam.php
+++ changeParam.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST['param']) && isset($_POST['value'])) {
-	$params = Array('iQuality', 'iScreenScale', 'iSound', 'iFps');
+	$params = Array('iQuality', 'iScreenScale', 'bMusic', 'iSfx');
 	setcookie($params[$_POST['param']], $_POST['value'], 4294967295,'/');
 	echo 1;
 }
