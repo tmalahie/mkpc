923912afa5bc1427712d5e04ba067cb3f56dc56f
Timothé Malahieude <timothe.malahieude@ecl14.ec-lyon.fr>
2020-08-30 15:38:46 +0200

    v75

--- mk/main.php
+++ mk/main.php
@@ -2,5 +2,5 @@
 if ($_SERVER['HTTP_HOST'] !== 'mkpc.malahieude.net')
 	echo '<script type="text/javascript" src="scripts/mk.js?reload=5"></script>';
 else
-	echo '<script type="text/javascript" src="scripts/mk.v74.js"></script>';
+	echo '<script type="text/javascript" src="scripts/mk.v75.js"></script>';
 ?>
