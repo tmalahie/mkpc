a94feb7d23b4460bd3c0d9d3dfa496026918461d
Timothé Malahieude <timothe.malahieude@ecl14.ec-lyon.fr>
2020-10-26 23:45:28 +0100

    v7b

--- mk/main.php
+++ mk/main.php
@@ -2,5 +2,5 @@
 if ($_SERVER['HTTP_HOST'] !== 'mkpc.malahieude.net')
 	echo '<script type="text/javascript" src="scripts/mk.js"></script>';
 else
-	echo '<script type="text/javascript" src="scripts/mk.v7a.js"></script>';
+	echo '<script type="text/javascript" src="scripts/mk.v7b.js"></script>';
 ?>
