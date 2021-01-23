6956dd479e696074530e8a4ffed0f89b339dc416
Timothé Malahieude <timothe.malahieude@ecl14.ec-lyon.fr>
2021-01-23 12:15:40 +0100

    Cache clear

--- mk/main.php
+++ mk/main.php
@@ -2,5 +2,5 @@
 if (($_SERVER['HTTP_HOST'] !== 'mkpc.malahieude.net') || isset($_GET['metakey']))
 	echo '<script type="text/javascript" src="scripts/mk.js?reload=1"></script>';
 else
-	echo '<script type="text/javascript" src="scripts/mk.v85.js"></script>';
+	echo '<script type="text/javascript" src="scripts/mk.v86.js"></script>';
 ?>
