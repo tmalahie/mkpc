1e2501330275cac85d5aa1561588dc8d1491eee5
Timothé Malahieude <timothe.malahieude@ecl14.ec-lyon.fr>
2021-02-21 23:01:35 +0100

    v88

--- mk/main.php
+++ mk/main.php
@@ -2,5 +2,5 @@
 if (($_SERVER['HTTP_HOST'] !== 'mkpc.malahieude.net') || isset($_GET['metakey']))
 	echo '<script type="text/javascript" src="scripts/mk.js?reload=1"></script>';
 else
-	echo '<script type="text/javascript" src="scripts/mk.v87.js"></script>';
+	echo '<script type="text/javascript" src="scripts/mk.v88.js"></script>';
 ?>
