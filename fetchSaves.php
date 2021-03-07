d686a5528401af7c4fb3d4b4f369fa72e98273b4
Timothé Malahieude <timothe.malahieude@ecl14.ec-lyon.fr>
2021-03-07 15:30:18 +0100

    Keep fixing errors for PHP 8

--- fetchSaves.php
+++ fetchSaves.php
@@ -8,8 +8,8 @@ else {
 	mysql_query('INSERT INTO `mksaves` VALUES("'. $identifiants[0] .'","'. $identifiants[1] .'","'. $identifiants[2] .'","'. $identifiants[3] .'","'. mysql_real_escape_string($mkSaves) .'")');
 }
 $total1 = 0;
-for ($i=0;$i<14;$i++)
-	$total1 += $mkSaves[$i];
+foreach ($mkSaves as $mkSave)
+    $total1 += $mkSave;
 $getMkSave2 = mysql_fetch_array(mysql_query('SELECT SUM(score) AS totalScore FROM `mkwins` WHERE identifiant="'. $identifiants[0] .'" AND identifiant2="'. $identifiants[1] .'" AND identifiant3="'. $identifiants[2] .'" AND identifiant4="'. $identifiants[3] .'"'));
 $total2 = $getMkSave2['totalScore'];
 ?>
