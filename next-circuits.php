56abff9641e25ff929429c35bfabdf0d5139fc14
Timothé Malahieude <timothe.malahieude@ecl14.ec-lyon.fr>
2021-04-09 23:34:32 +0200

    Yes another warning fixes

--- next-circuits.php
+++ next-circuits.php
@@ -2,19 +2,17 @@
 if (isset($_POST['page'])) {
 	include('initdb.php');
 	include('utils-circuits.php');
-	$id = $_POST['id'];
 	$page = +$_POST['page'];
 	$tri = isset($_POST['tri']) ? $_POST['tri']:0;
 	$nom = isset($_POST['nom']) ? stripslashes($_POST['nom']):'';
 	$auteur = isset($_POST['auteur']) ? stripslashes($_POST['auteur']):'';
 	$type = isset($_POST['type']) ? $_POST['type']:'';
+	$pids = null;
 	if (isset($_POST['user'])) {
 		$user = $_POST['user'];
 		if ($getProfile = mysql_fetch_array(mysql_query('SELECT identifiant,identifiant2,identifiant3,identifiant4 FROM `mkprofiles` WHERE id="'. $user .'"')))
 			$pids = array($getProfile['identifiant'],$getProfile['identifiant2'],$getProfile['identifiant3'],$getProfile['identifiant4']);
 	}
-	else
-		$user = '';
 	include('creations-params.php');
 	if ($type !== '') {
 		$aCircuits = array($aCircuits[$type]);
