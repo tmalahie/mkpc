7b84344a98a232cee51d3a8f53733fbc8ced4c11
Timothé Malahieude <timothe.malahieude@ecl14.ec-lyon.fr>
2021-05-12 00:00:55 +0200

    Get rid of testing library

--- docker/test/register.test.js
+++ docker/test/register.test.js
@@ -1,7 +1,5 @@
-const puppeteer = require('puppeteer')
-require('pptr-testing-library/extend')
-const { queries } = require('pptr-testing-library')
-const { getByText } = queries
+const puppeteer = require('puppeteer');
+const { getByText, getByLabelText, getByValue, getLinkByText } = require("./utils.js");
 
 const BASE_HREF = "http://localhost:8080";
 let browser, page;
@@ -17,7 +15,7 @@ beforeAll(async () => {
 test('Should go to the forum', async () => {
     await page.goto(BASE_HREF);
     const $nav = await page.$("nav");
-    const $forum = await getByText($nav, "Forum");
+    const $forum = await getLinkByText($nav, "Forum");
     await $forum.click();
     await page.waitForNavigation({ waitUntil: 'networkidle0' });
     const url = await page.url();
@@ -25,8 +23,7 @@ test('Should go to the forum', async () => {
 });
 
 test('Should go to the register page', async () => {
-    const $document = await page.getDocument();
-    const $register = await $document.getByText("Register");
+    const $register = await getLinkByText(page, "Register");
     await $register.click();
     await page.waitForNavigation({ waitUntil: 'networkidle0' });
     const url = await page.url();
@@ -34,25 +31,23 @@ test('Should go to the register page', async () => {
 });
 
 test('Should register', async () => {
-    const $document = await page.getDocument();
-    const $nick = await $document.getByLabelText(/choose a nick/i);
+    const $nick = await getByLabelText(page, "Choose a nick", {exact: false});
     await $nick.evaluate(nick => nick.value = "Wargor");
-    const $password = await $document.getByLabelText(/choose a password/i);
+    const $password = await getByLabelText(page, "Choose a password", {exact: false});
     await $password.evaluate(password => password.value = "aaaa");
-    const $confirm = await $document.getByLabelText(/re-enter password/i);
+    const $confirm = await getByLabelText(page, "Re-enter password", {exact: false});
     await $confirm.evaluate(confirm => confirm.value = "aaaa");
-    const $email = await $document.getByLabelText(/email address/i);
+    const $email = await getByLabelText(page, "Email address", {exact: false});
     await $email.evaluate(email => email.value = "email@example.com");
-    const $country = await $document.getByLabelText(/country/i);
+    const $country = await getByLabelText(page, "Country", {exact: false});
     await $country.evaluate(country => country.value = "fr");
 
-    const $submit = await $document.getByRole("button", {name: "Submit"});
+    const $submit = await getByValue(page, "Submit", {tag: "input"});
     await $submit.evaluate(submit => submit.form.submit());
 
     await page.waitForNavigation({ waitUntil: 'networkidle0' });
-    const $document2 = await page.getDocument();
-    await $document2.getByText("Welcome", {exact:false});
-    const $back = await $document2.getByRole("link", {name: "Back to the forum"});
+    await getByText(page, "Welcome", {exact:false});
+    const $back = await getLinkByText(page, "Back to the forum");
     await $back.click();
 
     await page.waitForNavigation({ waitUntil: 'networkidle0' });
@@ -61,8 +56,7 @@ test('Should register', async () => {
 });
 
 test('Should go to my profile', async () => {
-    const $document = await page.getDocument();
-    const $profile = await $document.getByRole("link", {name: "My profile"});
+    const $profile = await getLinkByText(page, "My profile");
     await $profile.click();
     await page.waitForNavigation({ waitUntil: 'networkidle0' });
     const url = await page.url();
@@ -70,9 +64,8 @@ test('Should go to my profile', async () => {
 });
 
 test('Should have correct information', async () => {
-    const $document = await page.getDocument();
-    await $document.getByText("Wargor's profile");
-    const $back = await $document.getByRole("link", {name: "Back to the forum", exact: false});
+    await getByText(page, "Wargor's profile");
+    const $back = await getLinkByText(page, "Back to the forum");
     await $back.click();
 
     await page.waitForNavigation({ waitUntil: 'networkidle0' });
@@ -81,8 +74,7 @@ test('Should have correct information', async () => {
 });
 
 test('Should log out', async () => {
-    const $document = await page.getDocument();
-    const $logout = await $document.getByRole("link", {name: "Log out"});
+    const $logout = await getLinkByText(page, "Log out");
     await $logout.click();
     await page.waitForNavigation({ waitUntil: 'networkidle0' });
     const url = await page.url();
@@ -92,10 +84,9 @@ test('Should log out', async () => {
 test('Should relog in', async () => {
     const passwordsAttempts = ["aaaaaa", "aaaa"];
     for (const passwordsAttempt of passwordsAttempts) {
-        const $document = await page.getDocument();
-        const $login = await $document.getByLabelText(/login/i);
+        const $login = await getByLabelText(page, "Login", {exact: false});
         await $login.evaluate(login => login.value = "Wargor");
-        const $password = await $document.getByLabelText(/password/i);
+        const $password = await getByLabelText(page, "Password", {exact: false});
         await $password.evaluate((password,passwordsAttempt) => password.value = passwordsAttempt, passwordsAttempt);
 
         await $password.evaluate(password => password.form.submit());
@@ -105,8 +96,7 @@ test('Should relog in', async () => {
         expect(url).toMatch(BASE_HREF+"/forum.php");
     }
 
-    const $document2 = await page.getDocument();
-    await $document2.getByRole("link", {name: "My profile"});
+    await getLinkByText(page, "My profile");
 });
 
 afterAll(async () => {
---
+++ docker/test/utils.js
@@ -0,0 +1,63 @@
+async function getSingleResult(results, args) {
+    if (!results.length)
+        throw Error('No result matches the specified selector: "'+ args[1] +'"');
+    if (results.length > 1) {
+        for (const result of results)
+            console.log((await result.getProperty("outerHTML"))+"");
+        throw Error('More than one result for the specified selector: "'+ args[1] +'"');
+    }
+    return results[0];
+}
+function toSingleResult(fn) {
+    return (...args) => fn(...args).then((res) => getSingleResult(res, args));
+}
+
+exports.getAllByText = async ($elt, text, options={}) => {
+    const tag = options.tag || "*";
+    let res;
+    if (options.exact !== false)
+        res = await $elt.$x('.//'+ tag +'[text() = "'+ text +'"]');
+    else
+        res = await $elt.$x('.//'+ tag +'[contains(., "'+ text +'")]');
+    let onlyNodes = [];
+    for (const elt of res) {
+        const html = await elt.getProperty("innerHTML").then(h => h.jsonValue());
+        if (!html.trim().startsWith("<")) // TODO this won't always work, find a cleaner way
+            onlyNodes.push(elt);
+    }
+    return onlyNodes;
+}
+exports.getByText = toSingleResult(exports.getAllByText);
+exports.getAllByLabelText = async ($elt, text, options) => {
+    const labels = await exports.getAllByText($elt, text, {
+        ...options,
+        tag: "label"
+    });
+    var res = [];
+    for (const label of labels) {
+        const forAttribute = await label.evaluate(l => l.getAttribute('for'));
+        let inputs;
+        if (forAttribute)
+            inputs = await $elt.$$("#"+forAttribute);
+        else
+            inputs = await label.$$("input");
+        if (inputs.length)
+            res.push(inputs[0]);
+    }
+    return res;
+}
+exports.getByLabelText = toSingleResult(exports.getAllByLabelText);
+
+exports.getAllByValue = async ($elt, text, options={}) => {
+    const tag = options.tag || "*";
+    return $elt.$x('.//'+ tag +'[@value = "'+ text +'"]');
+}
+exports.getByValue = toSingleResult(exports.getAllByValue);
+
+exports.getAllLinksByText = async ($elt, text, options={}) => {
+    return exports.getAllByText($elt, text, {
+        ...options,
+        tag: "a"
+    });
+}
+exports.getLinkByText = toSingleResult(exports.getAllLinksByText);
