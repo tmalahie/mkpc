2fea2dee53a29d96a362721c4962cc485145a887
tmalahie <t.malahieude@gmail.com>
2021-06-11 22:31:30 +0200

    v8d

--- mk/main.php
+++ mk/main.php
@@ -2,5 +2,5 @@
 if (($_SERVER['HTTP_HOST'] !== 'mkpc.malahieude.net') || isset($_GET['metakey']))
 	echo '<script type="text/javascript" src="scripts/mk.js?reload=4"></script>';
 else
-	echo '<script type="text/javascript" src="scripts/mk.v8c.js"></script>';
+	echo '<script type="text/javascript" src="scripts/mk.v8d.js"></script>';
 ?>
