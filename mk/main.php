f8f1e01a3264697a8d898026753e3588cbc81370
tmalahie <t.malahieude@gmail.com>
2021-06-11 20:32:02 +0200

    Cache clear

--- mk/main.php
+++ mk/main.php
@@ -1,6 +1,6 @@
 <?php
 if (($_SERVER['HTTP_HOST'] !== 'mkpc.malahieude.net') || isset($_GET['metakey']))
-	echo '<script type="text/javascript" src="scripts/mk.js?reload=7"></script>';
+	echo '<script type="text/javascript" src="scripts/mk.js?reload=6"></script>';
 else
 	echo '<script type="text/javascript" src="scripts/mk.v8c.js"></script>';
 ?>
