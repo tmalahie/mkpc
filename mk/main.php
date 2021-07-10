e03a9489d14b5af389adf586563316cf2471a3e5
tmalahie <t.malahieude@gmail.com>
2021-07-10 22:31:14 +0200

    v90

--- mk/main.php
+++ mk/main.php
@@ -2,5 +2,5 @@
 if (($_SERVER['HTTP_HOST'] !== 'mkpc.malahieude.net') || isset($_GET['metakey']))
 	echo '<script type="text/javascript" src="scripts/mk.js?reload=4"></script>';
 else
-	echo '<script type="text/javascript" src="scripts/mk.v8f.js"></script>';
+	echo '<script type="text/javascript" src="scripts/mk.v90.js"></script>';
 ?>
