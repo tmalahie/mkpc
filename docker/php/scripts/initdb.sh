4729dfcafd32f608471961887692a91552e5bc87
tmalahie <t.malahieude@gmail.com>
2021-07-13 23:45:55 +0200

    Update migration SQL file

--- docker/php/scripts/initdb.sh
+++ docker/php/scripts/initdb.sh
@@ -3,13 +3,13 @@ DIR=`dirname $(realpath $0)`
 echo "DROP DATABASE IF EXISTS mkpc;" > $DIR/setup.sql
 echo "CREATE DATABASE mkpc;" >> $DIR/setup.sql
 echo "USE mkpc;" >> $DIR/setup.sql
-dbIDs=$(cat $DIR/../../config/db.php | grep mysql_connect)
+dbIDs=$(cat $DIR/../../../config/db.php | grep mysql_connect)
 pat='.*mysql_connect(['\''"]\(.*\)['\''"], *['\''"]\(.*\)['\''"], *['\''"]\(.*\)['\''"]);.*'
 dbHost=$(echo $dbIDs | sed -n 's/'"$pat"'/\1/p')
 dbLogin=$(echo $dbIDs | sed -n 's/'"$pat"'/\2/p')
 dbPass=$(echo $dbIDs | sed -n 's/'"$pat"'/\3/p')
 pat='.*mysql_select_db(['\''"]\(.*\)['\''"]);.*'
-dbConn=$(cat $DIR/../../config/db.php | grep mysql_select_db)
+dbConn=$(cat $DIR/../../../config/db.php | grep mysql_select_db)
 dbName=$(echo $dbConn | sed -n 's/'"$pat"'/\1/p')
 mysqldump -h $dbHost -u $dbLogin -p$dbPass --no-data --skip-add-locks --single-transaction --compact $dbName  | sed 's/ AUTO_INCREMENT=[0-9]*//g' >> $DIR/setup.sql
 mysqldump -h $dbHost -u $dbLogin -p$dbPass --no-create-info --skip-add-locks --single-transaction --compact $dbName mkcategories mkcats mkcountries >> $DIR/setup.sql
