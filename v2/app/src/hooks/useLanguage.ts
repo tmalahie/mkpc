cd7bd5638e20cba4ff938557717b0fe76112a221
tmalahie <t.malahieude@gmail.com>
2021-11-11 16:44:26 +0100

    Better useLanguage hook

--- v2/app/src/hooks/useLanguage.ts
+++ v2/app/src/hooks/useLanguage.ts
@@ -1,3 +1,16 @@
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+function findLanguage() {
+  var res = +getCookie('language');
+  if (isNaN(res)) {
+    res = navigator.language.split('-')[0].startsWith("fr") ? 0 : 1;
+  }
+  return res;
+}
+var language = findLanguage();
 export function useLanguage() {
-  return 0; // TODO handle translations
+  return language;
 }
