128286492badd29c26337a672e5d009b8b8f0533
tmalahie <t.malahieude@gmail.com>
2021-11-15 22:40:32 +0100

    Small fixes in notification endpoint

--- v2/php-api/includes/circuits.php
+++ v2/php-api/includes/circuits.php
@@ -1,2 +1,8 @@
 <?php
 include('../../../circuitEscape.php');
+require_once('../includes/language.php');
+function getCircuitNames() {
+    $language = getLanguage();
+    include('../../../circuitNames.php');
+    return $circuitNames;
+}
