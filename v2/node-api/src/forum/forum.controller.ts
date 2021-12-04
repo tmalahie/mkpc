c1865784abc437aacc92aa4b460c20ddfc6c13b3
tmalahie <t.malahieude@gmail.com>
2021-12-04 22:08:52 +0100

    Add topics list page (wip)

--- v2/node-api/src/forum/forum.controller.ts
+++ v2/node-api/src/forum/forum.controller.ts
@@ -1,18 +1,19 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, Req } from '@nestjs/common';
 import { Auth } from '../auth/auth.decorator';
 import { I18n, I18nContext } from 'nestjs-i18n';
 import { AuthUser, GetUser } from 'src/user/user.decorator';
-import { EntityManager, MoreThan } from 'typeorm';
+import { EntityManager, In, MoreThan } from 'typeorm';
 import { Message } from './message.entity';
 import { Topic } from './topic.entity';
 import { Category } from './category.entity';
 import { Profile } from 'src/user/profile.entity';
 import { DateTime } from 'luxon';
+import { keyBy, pick } from 'lodash'
 @Controller("/forum")
 export class ForumController {
-  constructor(private em: EntityManager) {}
+  constructor(private em: EntityManager) { }
 
-  @Auth({loadRoles: true})
+  @Auth({ loadRoles: true })
   @Get("/topics")
   async getTopics(@GetUser() user: AuthUser, @I18n() i18n: I18nContext) {
     let topics = await this.em.find(Topic, {
@@ -31,16 +32,24 @@ export class ForumController {
         ...topics.filter(t => t.getLanguage() === "fr")
       ];
     }
+    const firstMessages = await this.em.find(Message, {
+      where: {
+        id: 1,
+        topic: In(topics.map(topic => topic.id))
+      },
+      relations: ["author"]
+    });
+    const firstMessagesByTopic = keyBy(firstMessages, "topic");
     const lastMessages = await Promise.all(topics.map(topic => this.em.findOne(Message, {
-        where: {
-            topic: topic.id
-        },
-        relations: ["author"],
-        order: {
-          id: "DESC"
-        }
-      })));
-    const data = topics.map((topic,i) => ({
+      where: {
+        topic: topic.id
+      },
+      relations: ["author"],
+      order: {
+        id: "DESC"
+      }
+    })));
+    const data = topics.map((topic, i) => ({
       id: topic.id,
       title: topic.title,
       nbMessages: topic.nbMessages,
@@ -48,9 +57,13 @@ export class ForumController {
         id: topic.category.id,
         name: topic.category.getName(i18n.detectedLanguage)
       },
+      firstMessage: {
+        author: firstMessagesByTopic[topic.id]?.author && pick(firstMessagesByTopic[topic.id].author, "id", "name"),
+        date: firstMessagesByTopic[topic.id]?.date
+      },
       lastMessage: {
-        author: lastMessages[i].author,
-        date: lastMessages[i].date
+        author: lastMessages[i]?.author && pick(lastMessages[i].author, "id", "name"),
+        date: lastMessages[i]?.date
       }
     }))
     return {
@@ -62,7 +75,7 @@ export class ForumController {
   async getCategories(@I18n() i18n: I18nContext) {
     const lang = i18n.detectedLanguage;
     const categories = await this.em.find(Category);
-    const sortedCategories = categories.sort((c1,c2) => c1.getOrder(lang) - c2.getOrder(lang))
+    const sortedCategories = categories.sort((c1, c2) => c1.getOrder(lang) - c2.getOrder(lang))
     const categoriesWithData = await Promise.all(sortedCategories.map(async category => {
       const nbTopics = await this.em.count(Topic, {
         where: {
@@ -77,13 +90,13 @@ export class ForumController {
           lastMessageDate: "DESC"
         }
       });
-      return { 
+      return {
         category,
         nbTopics,
         lastTopic
       };
     }))
-    const data = categoriesWithData.map(({category,nbTopics,lastTopic}) => ({
+    const data = categoriesWithData.map(({ category, nbTopics, lastTopic }) => ({
       id: category.id,
       name: category.getName(lang),
       description: category.getDescription(lang),
@@ -100,6 +113,23 @@ export class ForumController {
     return {
       data
     }
+  }
+
+  @Get("/categories/:id")
+  async getCategory(@I18n() i18n: I18nContext, @Param("id") id: number) {
+    const lang = i18n.detectedLanguage;
+    const category = await this.em.findOne(Category, {
+      where: {
+        id
+      }
+    });
+    if (!category)
+      throw new NotFoundException(`Category with id ${id} does not exist`);
+    return {
+      id: category.id,
+      name: category.getName(lang),
+      description: category.getDescription(lang)
+    };
   }
 
   @Get("/stats")
