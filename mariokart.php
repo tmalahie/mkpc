f77e4cac16765d298f787ae66660e2502d0a141b
tmalahie <t.malahieude@gmail.com>
2021-12-05 11:31:14 +0100

    Make game page HTML 5 (Need some tests...)

--- mariokart.php
+++ mariokart.php
@@ -2,8 +2,8 @@
 include('language.php');
 include('initdb.php');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $language ? 'en':'fr'; ?>" >
+<!DOCTYPE html>
+<html lang="<?php echo $language ? 'en':'fr'; ?>">
    <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="author" content="Timothé Malahieude" />
