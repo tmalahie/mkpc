055d110d4fb2f2c6a76d08a6146fc4092c0e1498
tmalahie <t.malahieude@gmail.com>
2021-12-29 22:03:09 +0100

    Add endpoint to get one news

--- v2/node-api/src/news/news.controller.ts
+++ v2/node-api/src/news/news.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
 import { Auth } from '../auth/auth.decorator';
 import { I18n, I18nContext } from 'nestjs-i18n';
 import { EntityManager } from 'typeorm';
@@ -10,9 +10,46 @@ import { AuthUser, GetUser } from 'src/user/user.decorator';
 export class NewsController {
   constructor(private em: EntityManager, private searchService: SearchService) { }
 
+  accessRestricted(news: News, user: AuthUser) {
+    if (news.status === NewsStatus.ACCEPTED)
+      return false;
+    if (news.author?.id === user.id)
+      return false;
+    if (user.roles.publisher)
+      return false;
+    return true;
+  }
+
+  @Auth({ loadRoles: true })
+  @Get("/:id")
+  async getNews(@GetUser() user: AuthUser, @I18n() i18n: I18nContext, @Param("id") id: number) {
+    const news = await this.em.findOne(News, id, {
+      relations: ["category", "author"]
+    });
+    if (!news || this.accessRestricted(news, user))
+      throw new NotFoundException(`News with id ${id} does not exist`);
+    return {
+      id: news.id,
+      title: news.title,
+      category: {
+        id: news.category.id,
+        name: news.category.getName(i18n.detectedLanguage),
+      },
+      status: news.status,
+      creationDate: news.creationDate,
+      publicationDate: news.publicationDate,
+      nbComments: news.nbComments,
+      author: news.author && {
+        id: news.author.id,
+        name: news.author.name
+      },
+      content: news.content
+    }
+  }
+
   @Auth({ loadRoles: true })
   @Post("/find")
-  async getNews(@GetUser() user: AuthUser, @I18n() i18n: I18nContext, @Body() params) {
+  async findNews(@GetUser() user: AuthUser, @I18n() i18n: I18nContext, @Body() params) {
     const { data: listNews, count } = await this.searchService.find({
       entity: News,
       params,
