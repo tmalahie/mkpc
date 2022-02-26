34ea6ef23fbe307ca3ce1906823c91afac4b0dcd
tmalahie <t.malahieude@gmail.com>
2022-02-26 10:09:25 +0100

    Optimize thumbnail generation

--- mappreview.php
+++ mappreview.php
@@ -15,19 +15,32 @@ if (isset($id)) {
 		$snes = ($map < 13);
 		$gba = ($map < 31);
 
+		$wImg = 600;
+		$hImg = 600;
+		$rImg = 1;
+		if ($isTemp && isset($w_ic) && isset($w_ic)) {
+			$rImg = $w_ic/$wImg;
+			$wImg = $w_ic;
+			$hImg = $h_ic;
+		}
+
 		if (in_array($map, array(48)))
-			$image = imagecreatetruecolor(600,600);
+			$image = imagecreatetruecolor($wImg,$hImg);
 		else
-			$image = imagecreate(600,600);
+			$image = imagecreate($wImg,$hImg);
 		if (isset($bgColors[$map])) {
 			$bg = $bgColors[$map];
 			imagecolorallocate($image, $bg[0], $bg[1], $bg[2]);
 
 			$pieces = mysql_query('SELECT * FROM `mkp` WHERE circuit="'.$id.'"');
+			$pW = 100*$rImg;
+			$pH = 100*$rImg;
 			while ($piece = mysql_fetch_array($pieces)) {
-				$i = 
 				$piececircuit = imagecreatefrompng('images/pieces/piececircuit'.$map.'_'.$piece['piece'].'.png');
-				imagecopy($image, $piececircuit, ($piece['id']%6)*100, floor($piece['id']/6)*100, 0, 0, 100, 100);
+				if ($rImg === 1)
+					imagecopy($image, $piececircuit, ($piece['id']%6)*$pW,floor($piece['id']/6)*$pH, 0,0, $pW,$pH);
+				else
+					imagecopyresampled($image, $piececircuit, ($piece['id']%6)*$pW,floor($piece['id']/6)*$pH, 0,0, $pW,$pH, 100,100);
 			}
 		}
 		$elements = Array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','o');
@@ -52,8 +65,12 @@ if (isset($id)) {
 				$cw = 0;
 				$ch = 0;
 			}
-			while ($getPiece = mysql_fetch_array($getPieces))
-				imagecopy($image, $piececircuit, $getPiece['x']-$cw,$getPiece['y']-$ch, 0, 0, $w,$h);
+			while ($getPiece = mysql_fetch_array($getPieces)) {
+				if ($rImg === 1)
+					imagecopy($image, $piececircuit, $getPiece['x']-$cw,$getPiece['y']-$ch, 0,0, $w,$h);
+				else
+					imagecopyresampled($image, $piececircuit, ($getPiece['x']-$cw)*$rImg,($getPiece['y']-$ch)*$rImg, 0,0, $w*$rImg,$h*$rImg, $w,$h);
+			}
 		}
 		foreach ($decorTypes[$map] as $i=>$decorType) {
 			$piececircuit = imagecreatefrompng('images/map_icons/'.$decorType.'.png');
@@ -68,7 +85,7 @@ if (isset($id)) {
 			$ch = round($rh/2);
 			$getPieces = mysql_query('SELECT x,y FROM `mkt` WHERE circuit="'.$id.'" AND t="'.$i.'"');
 			while ($getPiece = mysql_fetch_array($getPieces))
-				imagecopyresampled($image, $piececircuit, $getPiece['x']-$cw,$getPiece['y']-$ch, 0, 0, $rw,$rh, $w,$h);
+				imagecopyresampled($image, $piececircuit, ($getPiece['x']-$cw)*$rImg,($getPiece['y']-$ch)*$rImg, 0,0, $rw*$rImg,$rh*$rImg, $w,$h);
 		}
 
 		$ext2 = 'png';
