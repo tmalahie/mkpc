fe64e1cc4f85565a6a9934bca854d2073be7a7af
tmalahie <t.malahieude@gmail.com>
2022-06-08 22:45:19 +0200

    Put challenge recommandations topic  link in several pages of challenge editor

--- challengeEdit.php
+++ challengeEdit.php
@@ -199,7 +199,7 @@ elseif (empty($challenge) || ('pending_completion' === $challenge['status']) || 
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
-<link rel="stylesheet" href="styles/challenges.css?reload=1" />
+<link rel="stylesheet" href="styles/challenges.css" />
 <script type="text/javascript" src="scripts/jquery.min.js"></script>
 <?php
 if (empty($moderate))
--- challengeReco.php
+++ challengeReco.php
@@ -40,6 +40,7 @@ body {
                 <li>Challenge name with insults or inappropriate words.</li>
             </ul>
             <p>
+                To see a full list of rules your challenge should respect, you can read this <a class="pretty-link" target="_blank" href="topic.php?topic=7109">forum topic</a>.<br />
                 Also, please check that the difficulty you assigned to your challenge is relevant. Click <a class="pretty-link" href="helpDifficulty.html">here</a> to see recommendations about difficulty.
             </p>
             <?php
@@ -58,6 +59,7 @@ body {
                 <li>Nom de défi avec des insultes ou des mots obscènes</li>
             </ul>
             <p>
+                Pour voir la liste complète des règles que votre défi doit respecter, vous pouvez lire <a class="pretty-link" target="_blank" href="topic.php?topic=7109">ce topic</a> sur le forum.<br />
                 Vérifiez également que la difficulté qur vous avez attribuée à votre défi est pertinente. Cliquez <a class="pretty-link" href="aideDifficulty.html">ici</a> pour voir les recommandations sur la difficulté.
             </p>
             <?php
