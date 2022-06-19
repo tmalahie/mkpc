9e8ae1a6111f13f72e748a365c596015001bad41
tmalahie <timothe@skillup.co>
2022-06-19 21:12:27 +0000

    Show currently online players to home page

--- v2/php-api/includes/api.php
+++ v2/php-api/includes/api.php
@@ -1,9 +1,16 @@
 <?php
 function getJsonBody() {
-    return json_decode(file_get_contents('php://input'));
+  return json_decode(file_get_contents('php://input'));
 }
 function renderResponse($response, $code = 200) {
-    http_response_code($code);
-    header('Content-Type: application/json');
-    echo json_encode($response);
+  http_response_code($code);
+  header('Content-Type: application/json');
+  echo json_encode($response);
+}
+function compactObject($arr) {
+  foreach ($arr as $key => $value) {
+    if ($value === null)
+      unset($arr[$key]);
+  }
+  return $arr;
 }
