551db5c464e5a734364660cab9cbf48f65dee78c
tmalahie <t.malahieude@gmail.com>
2022-06-24 22:59:42 +0200

    Implement API endpoint for time trial global ranking page

--- v2/node-api/src/time-trial/time-trial.controller.ts
+++ v2/node-api/src/time-trial/time-trial.controller.ts
@@ -1,10 +1,11 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
-import { EntityManager, FindOneOptions, In, LessThan } from 'typeorm';
+import { EntityManager, FindOneOptions, In, LessThan, Raw } from 'typeorm';
 import { Record } from './record.entity';
 import { Ranking } from './ranking.entity';
 import { CircuitService } from '../track-builder/circuit.service';
-import { SearchService, EQUALITY_SEARCH } from '../search/search.service';
+import { SearchService, EQUALITY_SEARCH, SearchType } from '../search/search.service';
 import { pick } from 'lodash';
+import { User } from 'src/user/user.entity';
 
 @Controller("/time-trial")
 export class TimeTrialController {
@@ -80,20 +81,67 @@ export class TimeTrialController {
   @Post("/leaderboard")
   async getLeaderboard(@Body() params) {
     const ccFilter = +params.cc || 150;
-    const leaderboard = await this.em.find(Ranking, {
-      where: {
-        class: ccFilter,
-        player: {
-          deleted: false
+    const baseWhere: FindOneOptions<Ranking>["where"] = {
+      class: ccFilter,
+      player: {
+        deleted: false
+      }
+    };
+    let where = baseWhere;
+    if (params) {
+      if (params.name) {
+        const user = await this.em.findOne(User, {
+          where: {
+            name: params.name
+          }
+        });
+        where = {
+          ...where,
+          player: undefined
         }
-      },
-      order: {
-        score: "DESC"
+        params.filters = [
+          ...(params.filters || []), {
+            "key": "player",
+            "operator": "=",
+            "value": user?.id ?? 0
+          }
+        ];
+      }
+      else {
+        params.sort = params.sort || {
+          "key": "score",
+          "order": "desc"
+        }
+      }
+    }
+    let skip = +params.paging?.offset || 0;
+    const { data: leaderboard, count } = await this.searchService.find({
+      entity: Ranking,
+      params,
+      where,
+      rules: {
+        allowedFilters: {
+          player: [SearchType.EQUALS],
+        },
+        allowedOrders: ["player", "score"],
+        defaultOrder: {
+          player: "ASC"
+        },
+        canReturnCount: true,
+        maxResults: 20
       },
       relations: ["player", "player.profile", "player.profile.country"],
-      take: 20
     });
-    const skip = 0;
+    if (params.name && leaderboard.length) {
+      const usersBefore = await this.em.count(Ranking, {
+        where: {
+          score: Raw((alias) => `(${alias} > :pts OR (${alias} = :pts AND player<:id))`, { id: leaderboard[0].id, pts: leaderboard[0].score }),
+          ...baseWhere
+        },
+        relations: ["player"]
+      });
+      skip = usersBefore;
+    }
     const data = leaderboard.map((ranking, i) => ({
       id: ranking.player.id,
       name: ranking.player.name,
@@ -101,6 +149,6 @@ export class TimeTrialController {
       rank: i + skip + 1,
       country: ranking.player.profile.country && pick(ranking.player.profile.country, ["id", "code"])
     }));
-    return { data, count: data.length };
+    return { data, count };
   }
 }
