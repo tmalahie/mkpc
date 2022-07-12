057a1706442fffed9c205c82cf15b99021b3bdbf
tmalahie <t.malahieude@gmail.com>
2022-07-12 22:13:20 +0200

    vc6

--- mk/main.php
+++ mk/main.php
@@ -2,5 +2,5 @@
 if (($_SERVER['HTTP_HOST'] !== 'mkpc.malahieude.net') || isset($_GET['metakey']))
 	echo '<script type="text/javascript" src="scripts/mk.js?reload=2"></script>';
 else
-	echo '<script type="text/javascript" src="scripts/mk.vc5.js"></script>';
+	echo '<script type="text/javascript" src="scripts/mk.vc6.js"></script>';
 ?>
