9502f1523d86d2d7ca1f42acf399a6adc430f9cf
tmalahie <t.malahieude@gmail.com>
2022-08-26 21:14:27 +0200

    Small fixes

--- public_links.php
+++ public_links.php
@@ -2,10 +2,7 @@
 $getPublicLinks = mysql_query('SELECT id,rules FROM `mkgameoptions` WHERE public');
 $publicLinkIds = array(0);
 $publicLinksData = array(
-	(object)array(
-		'team' => 0,
-		'friendly' => 0
-	)
+	(object)array()
 );
 while ($getPublicLink = mysql_fetch_array($getPublicLinks)) {
 	$publicLinksData[] = json_decode($getPublicLink['rules']);
