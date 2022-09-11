d57e9077a4af25ec9c3a0f8b226d7006ffd23725
tmalahie <t.malahieude@gmail.com>
2022-09-11 20:04:46 +0200

    Update setup.sql

--- docker/php/scripts/setup.sql
+++ docker/php/scripts/setup.sql
@@ -193,6 +193,26 @@ CREATE TABLE `mkbans` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mkbglayers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bg` int(11) NOT NULL,
+  `ordering` int(11) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mkbgs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `creation_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `identifiant` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `mkbrowsers` (
   `player` int(11) NOT NULL,
   `browser` varchar(255) NOT NULL,
