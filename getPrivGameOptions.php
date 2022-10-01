1ce991dd0fae9ba04ed4bb3f349dfb59ba0c0942
tmalahie <t.malahieude@gmail.com>
2022-10-01 20:37:22 +0200

    Fix import from link not working if no game options

--- getPrivGameOptions.php
+++ getPrivGameOptions.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_POST['key'])) {
 	include('initdb.php');
-	if ($privateLinkData = mysql_fetch_array(mysql_query('SELECT * FROM `mkgameoptions` WHERE id="'.$_POST['key'].'"')))
+	if ($privateLinkData = mysql_fetch_array(mysql_query('SELECT IFNULL(o.rules,"{}") AS rules FROM `mkprivgame` p LEFT JOIN `mkgameoptions` o ON p.id=o.id WHERE p.id="'.$_POST['key'].'"')))
 		echo $privateLinkData['rules'];
 	mysql_close();
 }
