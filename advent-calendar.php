f39c5bf583415fc0a4256a6bc51da0a3fea73d1c
tmalahie <t.malahieude@gmail.com>
2022-11-30 22:56:50 +0100

    Update description

--- advent-calendar.php
+++ advent-calendar.php
@@ -8,7 +8,7 @@ include('initdb.php');
 <head>
 <title>Calendrier de l'Avent - Mario Kart PC</title>
 <?php
-$hdescription = $language ? "It's Christmas on Mario Kart PC! To celebrate, this unique event gives you access to 1 challenge per day! Win as many challenges as possible to earn lots of challenge points!" : "C'est Noël sur Mario Kart PC ! Pour fêter ça, cet événement inédit vous donne accès à 1 défi par jour ! Remportez un maximum de défis pour gagner tout plein de points défis !";
+$hdescription = $language ? "It's Christmas on Mario Kart PC! To celebrate, this unique event gives you access to 1 challenge per day! Win as many challenges as possible to earn up to 400+ challenge points!" : "C'est Noël sur Mario Kart PC ! Pour fêter ça, cet événement inédit vous donne accès à 1 défi par jour ! Remportez un maximum de défis pour gagner jusqu'à 400+ points défis !";
 include('heads.php');
 $year = isset($_GET['y']) ? $_GET['y'] : 2022;
 $over = false;
