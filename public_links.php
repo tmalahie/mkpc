0a66cfdcc30d98590469f250dc969a12e9b02924
tmalahie <t.malahieude@gmail.com>
2023-03-22 22:17:28 +0100

    small fix in public links

--- public_links.php
+++ public_links.php
@@ -1,11 +1,11 @@
 <?php
-$getPublicLinks = mysql_query('SELECT id,rules FROM `mkgameoptions` WHERE public');
+$getPublicLinks = mysql_query('SELECT id,rules FROM `mkgameoptions` WHERE public ORDER BY id');
 $publicLinkIds = array(0);
 $publicLinksData = array(
 	(object)array()
 );
 while ($getPublicLink = mysql_fetch_array($getPublicLinks)) {
-	$publicLinksData[] = json_decode($getPublicLink['rules']);
+	$publicLinksData[$getPublicLink['id']] = json_decode($getPublicLink['rules']);
 	$publicLinkIds[] = $getPublicLink['id'];
 }
 $publicLinksString = implode(',',$publicLinkIds);
