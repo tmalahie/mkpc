457fa1d53091b6b57fe174863655e3f9c523aedb
tmalahie <t.malahieude@gmail.com>
2023-03-26 13:07:35 +0200

    v121

--- mk/main.php
+++ mk/main.php
@@ -2,5 +2,5 @@
 if (($_SERVER['HTTP_HOST'] !== 'mkpc.malahieude.net') || isset($_GET['metakey']))
 	echo '<script type="text/javascript" src="scripts/mk.js"></script>';
 else
-	echo '<script type="text/javascript" src="scripts/mk.v120.js"></script>';
+	echo '<script type="text/javascript" src="scripts/mk.v121.js"></script>';
 ?>
