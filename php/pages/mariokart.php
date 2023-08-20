b98d4e16e00a900ef14566bc5168b63e66f54de0
tmalahie <t.malahieude@gmail.com>
2023-08-20 20:26:41 +0200

    Fix WS collisions again

--- php/pages/mariokart.php
+++ php/pages/mariokart.php
@@ -75,7 +75,7 @@ var NBCIRCUITS = <?php echo $nbVSCircuits; ?>;
 <?php
 mysql_close();
 ?>
-<script type="text/javascript" src="scripts/maps.php?reload=1"></script>
+<script type="text/javascript" src="scripts/maps.php?reload=2"></script>
 <?php include('../includes/mk/main.php') ?>
 <script type="text/javascript">
 document.addEventListener("DOMContentLoaded", MarioKart);
--- php/pages/online.php
+++ php/pages/online.php
@@ -127,7 +127,7 @@ if (isset($privateLink)) {
 <?php
 if (!$isCustom) {
 	?>
-<script type="text/javascript" src="scripts/maps.php?reload=1"></script>
+<script type="text/javascript" src="scripts/maps.php?reload=2"></script>
 	<?php
 }
 ?>
