6694a1376afda2c29a72a236c9ff6bb7958589ca
tmalahie <t.malahieude@gmail.com>
2023-08-29 13:00:26 +0200

    Prevent banned users to follow users

--- php/pages/follow-user.php
+++ php/pages/follow-user.php
@@ -5,8 +5,12 @@ if (isset($_GET['user'])) {
 		include('../includes/initdb.php');
 		if (mysql_fetch_array(mysql_query('SELECT * FROM `mkjoueurs` WHERE id="'. $_GET['user'] .'"'))) {
 			if (isset($_GET['follow'])) {
-				mysql_query('INSERT IGNORE INTO `mkfollowusers` VALUES("'. $id .'","'. $_GET['user'] .'",NULL)');
-				mysql_query('INSERT INTO `mknotifs` SET type="new_followuser", user="'. $_GET['user'] .'", link="'.$id.'"');
+				$getBanned = mysql_fetch_array(mysql_query('SELECT banned FROM `mkjoueurs` WHERE id="'. $id .'"'));
+				$isBanned = $getBanned && $getBanned['banned'];
+				if (!$isBanned) {
+					mysql_query('INSERT IGNORE INTO `mkfollowusers` VALUES("'. $id .'","'. $_GET['user'] .'",NULL)');
+					mysql_query('INSERT INTO `mknotifs` SET type="new_followuser", user="'. $_GET['user'] .'", link="'.$id.'"');
+				}
 			}
 			else
 				mysql_query('DELETE FROM `mkfollowusers` WHERE follower="'. $id .'" AND followed="'. $_GET['user'] .'"');
