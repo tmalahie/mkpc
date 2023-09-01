0e800d18ae74d6f323f8ec3c3ae6ba29eb7f01d2
tmalahie <t.malahieude@gmail.com>
2023-09-02 00:11:49 +0200

    Add referrer to game crash logs

--- php/api/logGameCrash.php
+++ php/api/logGameCrash.php
@@ -5,7 +5,10 @@ if (isset($_POST["error"])) {
 	include('../includes/session.php');
 	include('../includes/initdb.php');
     if (!$id) $id = 0;
-    mysql_query('INSERT INTO mkgamecrash SET player="'.$id.'",identifiant="'.$identifiants[0].'",stack="'.$_POST["error"].'"');
+    $referrer = '';
+    if (isset($_SERVER['HTTP_REFERER']))
+        $referrer = $_SERVER['HTTP_REFERER'];
+    mysql_query('INSERT INTO mkgamecrash SET player="'.$id.'",identifiant="'.$identifiants[0].'",stack="'.$_POST["error"].'",referrer="'.mysql_real_escape_string($referrer).'"');
     mysql_close();
     echo 1;
 }
