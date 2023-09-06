995180094b8f633a853aafe9dcd65a425ad57917
tmalahie <t.malahieude@gmail.com>
2023-09-06 22:16:49 +0200

    Prevent banned users to follow topics

--- php/api/follow.php
+++ php/api/follow.php
@@ -6,10 +6,14 @@ if (isset($_POST['topic']) && is_numeric($_POST['topic'])) {
 		include('../includes/initdb.php');
 		if (mysql_fetch_array(mysql_query('SELECT * FROM `mktopics` WHERE id="'. $_POST['topic'] .'"'))) {
 			if (isset($_POST['follow'])) {
-				mysql_query('INSERT IGNORE INTO `mkfollowers` VALUES("'. $id .'","'. $_POST['topic'] .'")');
-				if ($getFirstMessage = mysql_fetch_array(mysql_query('SELECT auteur FROM `mkmessages` WHERE topic="'. $_POST['topic'] .'" AND id=1 LIMIT 1'))) {
-					if ($getFirstMessage['auteur'] != $id)
-						mysql_query('INSERT INTO `mknotifs` SET type="new_followtopic", user="'. $getFirstMessage['auteur'] .'", link="'.$_POST['topic'].','.$id.'"');
+				$getBanned = mysql_fetch_array(mysql_query('SELECT banned FROM `mkjoueurs` WHERE id="'. $id .'"'));
+				$isBanned = $getBanned && $getBanned['banned'];
+				if (!$isBanned) {
+					mysql_query('INSERT IGNORE INTO `mkfollowers` VALUES("'. $id .'","'. $_POST['topic'] .'")');
+					if ($getFirstMessage = mysql_fetch_array(mysql_query('SELECT auteur FROM `mkmessages` WHERE topic="'. $_POST['topic'] .'" AND id=1 LIMIT 1'))) {
+						if ($getFirstMessage['auteur'] != $id)
+							mysql_query('INSERT INTO `mknotifs` SET type="new_followtopic", user="'. $getFirstMessage['auteur'] .'", link="'.$_POST['topic'].','.$id.'"');
+					}
 				}
 			}
 			else
