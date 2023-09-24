553c56f712c2473c10813c6888f167ed1eb4e0f7
tmalahie <t.malahieude@gmail.com>
2023-09-24 20:51:11 +0200

    Fix index issue

--- php/api/otherghosts.php
+++ php/api/otherghosts.php
@@ -5,7 +5,7 @@ if (isset($_POST['map'])) {
 	$map = $_POST['map'];
 	$type = isset($_POST['type']) ? $_POST['type'] : '';
 	$cc = isset($_POST['cc']) ? $_POST['cc'] : 150;
-	$getPersos = mysql_query('SELECT g.id,g.perso,g.time,g.lap_times,r.name FROM `mkghosts` g LEFT JOIN `mkrecords` r ON g.identifiant=r.identifiant AND g.identifiant2=r.identifiant2 AND g.identifiant3=r.identifiant3 AND g.identifiant4=r.identifiant4 AND g.perso=r.perso AND g.time=r.time AND r.class=g.class AND r.circuit=g.circuit AND r.type=g.type WHERE g.class="'. $cc .'" AND g.type="'. $type .'" AND g.circuit="'. $map .'" GROUP BY g.id,g.perso,g.time');
+	$getPersos = mysql_query('SELECT g.id,g.perso,g.time,g.lap_times,r.name FROM `mkghosts` g LEFT JOIN `mkrecords` r FORCE INDEX FOR JOIN (`identifiant`) ON g.identifiant=r.identifiant AND g.identifiant2=r.identifiant2 AND g.identifiant3=r.identifiant3 AND g.identifiant4=r.identifiant4 AND g.perso=r.perso AND g.time=r.time AND r.class=g.class AND r.circuit=g.circuit AND r.type=g.type WHERE g.class="'. $cc .'" AND g.type="'. $type .'" AND g.circuit="'. $map .'" GROUP BY g.id,g.perso,g.time');
 	echo '[';
 	$colon = '';
 	while ($getPerso = mysql_fetch_array($getPersos)) {
