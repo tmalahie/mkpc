2834b35e458b587ed1dfb0b1fbf0f762c5f6b720
tmalahie <t.malahieude@gmail.com>
2023-12-01 23:25:17 +0100

    Cache clear

--- php/pages/online.php
+++ php/pages/online.php
@@ -122,7 +122,7 @@ if (isset($privateLink)) {
 ?>
 
 <?php include('../includes/c_mariokart.php'); ?>
-<link rel="stylesheet" media="screen" type="text/css" href="styles/mk-online.css?reload=1" />
+<link rel="stylesheet" media="screen" type="text/css" href="styles/mk-online.css" />
 
 <?php
 if (!$isCustom) {
