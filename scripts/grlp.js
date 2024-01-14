26408d1521f25093fa42b55ac37899d8aca58b54
tmalahie <t.malahieude@gmail.com>
2024-01-14 21:58:22 +0100

    Make grlp smarter

--- scripts/grlp.js
+++ scripts/grlp.js
@@ -2,17 +2,31 @@ var $grlp = document.querySelector(".google-revocation-link-placeholder");
 if ($grlp)
 	$grlp.remove();
 else if (window.adsbygoogle) {
-	var observer = new MutationObserver(() => {
+	function observeGrlp() {
+		var observer = new MutationObserver(() => {
+			$grlp = document.querySelector(".google-revocation-link-placeholder");
+			if (!$grlp) return;
+			$grlp.remove();
+			observer.disconnect();
+			window.removeEventListener("scroll", handleScrollGrlp);
+		});
+
+		observer.observe(document.body, {
+			childList: true,
+		});
+
+		setTimeout(function() {
+			observer.disconnect();
+		}, 5000);
+	}
+	function handleScrollGrlp() {
 		$grlp = document.querySelector(".google-revocation-link-placeholder");
-		if (!$grlp) return;
-		$grlp.remove();
-		observer.disconnect();
-	});
-	
-	observer.observe(document.body, {
-		childList: true,
-	});
-	setTimeout(function() {
-		observer.disconnect();
-	}, 5000);
+		if ($grlp)
+			$grlp.remove();
+		else
+			observeGrlp();
+		window.removeEventListener("scroll", handleScrollGrlp);
+	}
+	observeGrlp();
+	window.addEventListener("scroll", handleScrollGrlp);
 }
