380e023dc9973d52ca047ad7bd16e5825313e83e
tmalahie <t.malahieude@gmail.com>
2024-08-11 19:25:38 +0200

    Cache clear (again)

--- php/pages/course.php
+++ php/pages/course.php
@@ -61,7 +61,7 @@ if (isset($_GET['i'])) {
 		include('../includes/o_xhr.php');
 		?>
 		<script type="text/javascript" src="scripts/editor.js?reload=2"></script>
-		<script type="text/javascript" src="scripts/course.js"></script>
+		<script type="text/javascript" src="scripts/course.js?reload=1"></script>
 	</head>
 	<body onkeydown="handleKeySortcuts(event)" onbeforeunload="return handlePageExit()" class="editor-body">
 		<div id="editor-wrapper"<?php if (!$hasWriteGrants) echo ' class="readonly"'; ?>  oncontextmenu="handleCtxmenu(event)" onmousemove="handleMove(event)" onclick="handleClick(event)">
