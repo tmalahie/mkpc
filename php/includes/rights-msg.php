a7589afaafc18314a73130e2d7965dc718a503fb
Pianta <113540867+PiantaMK@users.noreply.github.com>
2025-04-05 17:33:52 +0200

    ban expiry date

--- php/includes/rights-msg.php
+++ php/includes/rights-msg.php
@@ -2,58 +2,35 @@
 $getBanned = mysql_query('SELECT banned FROM `mkjoueurs` WHERE id="'. $id .'"');
 $isBanned = mysql_fetch_array($getBanned);
 if ($isBanned && $isBanned['banned']) {
-	if ($isBanned['banned'] == 1)
-		include('ban_ip.php');
-	if ($language) {
-		?>
-		<p class="warning"><?php
-			$getBanMsg = mysql_fetch_array(mysql_query('SELECT msg FROM `mkbans` WHERE player='. $id));
-			if ($getBanMsg && $getBanMsg['msg']) {
-				?>
-				<p class="warning">You have been banned for the following reason:<br />
-				<strong style="background-color: #FCC;display:inline-block;margin:5px;padding:5px"><?php
-				$getBanMsg = mysql_fetch_array(mysql_query('SELECT msg FROM `mkbans` WHERE player='. $id));
-				echo nl2br(htmlspecialchars($getBanMsg['msg']));
-				?></strong><br />
-				Therefore, you can not post messages until further notice.</p>
-				<?php
-			}
-			else {
-				?>
-				You have been banned temporarily because of inappropriate behavior.<br />
-				Therefore, you can not post messages until further notice.<br />
-				Be careful : in case of recurrence, your account will be deleted.
-				<?php
-			}
-			?>
-		</p>
-		<?php
-	}
-	else {
-		?>
-		<p class="warning"><?php
-			$getBanMsg = mysql_fetch_array(mysql_query('SELECT msg FROM `mkbans` WHERE player='. $id));
-			if ($getBanMsg && $getBanMsg['msg']) {
-				?>
-				Vous avez été banni pour la raison suivante :<br />
-				<strong style="background-color: #FCC;display:inline-block;margin:5px;padding:5px"><?php
-				$getBanMsg = mysql_fetch_array(mysql_query('SELECT msg FROM `mkbans` WHERE player='. $id));
-				echo nl2br(htmlspecialchars($getBanMsg['msg']));
-				?></strong><br />
-				Par conséquent, vous ne pourrez plus poster de message jusqu'à nouvel ordre.
-				<?php
-			}
-			else {
-				?>
-				Vous avez été banni temporairement suite à un comportement innaproprié sur le site.<br />
-				Par conséquent, vous ne pouvez plus poster de message jusqu'à nouvel ordre.<br />
-				Attention : en cas de récidive, votre compte sera supprimé définitivement.
-				<?php
-			}
-			?>
-		</p>
-		<?php
-	}
+    if ($isBanned['banned'] == 1)
+        include('ban_ip.php');
+    $getBanDetails = mysql_fetch_array(mysql_query('SELECT msg, end_date FROM `mkbans` WHERE player='. $id));
+    $banReason = $getBanDetails['msg'] ?? null;
+    $banEndDate = $getBanDetails['end_date'] ?? null;
+    $currentDate = date('Y-m-d');
+    if ($language) {
+        ?>
+        <p class="warning">
+            <strong>You have been banned!</strong><br />
+            <?php if ($banReason): ?>
+                Reason: <strong style="background-color: #FCC;display:inline-block;margin:5px;padding:5px"><?php echo nl2br(htmlspecialchars($banReason)); ?></strong><br />
+            <?php endif; ?>
+            Ends at: <strong><?php echo $banEndDate ? htmlspecialchars($banEndDate) : 'Permanent'; ?></strong><br />
+            Be careful: in case of recurrence, your account will be deleted.<br />
+        </p>
+        <?php
+    } else {
+        ?>
+        <p class="warning">
+            <strong>Vous avez été banni!</strong><br />
+            <?php if ($banReason): ?>
+                Raison: <strong style="background-color: #FCC;display:inline-block;margin:5px;padding:5px"><?php echo nl2br(htmlspecialchars($banReason)); ?></strong><br />
+            <?php endif; ?>
+            Fin: <strong><?php echo $banEndDate ? htmlspecialchars($banEndDate) : 'Permanent'; ?></strong><br />
+            Attention: en cas de récidive, votre compte sera supprimé définitivement.<br />
+        </p>
+        <?php
+    }
 }
 elseif ($getWarnMsg = mysql_fetch_array(mysql_query('SELECT msg FROM mkwarns WHERE player='. $id .' AND msg!=""'))) {
 	?>
