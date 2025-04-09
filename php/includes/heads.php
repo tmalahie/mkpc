5b44efefec2b9df1802d8ec0218a96de0359cddb
Pianta <113540867+PiantaMK@users.noreply.github.com>
2025-04-09 21:00:07 +0200

    minor php

--- php/includes/heads.php
+++ php/includes/heads.php
@@ -1,29 +1,36 @@
 <?php
-if (!isset($hthumbnail))
-	$hthumbnail = 'https://mkpc.malahieude.net/images/screenshots/ss1.png';
+// Set default thumbnail if not provided
+if (!isset($hthumbnail)) {
+    $hthumbnail = 'https://mkpc.malahieude.net/images/screenshots/ss1.png';
+}
+
+// Include external description utility and format the description
 require_once('utils-description.php');
 $hdescription = formatDescription($hdescription);
+
+// Set language-based keywords
+$keywords = $language ? 
+    'Mario, Kart, PC, game, race, free game, multiplayer, circuits editor' : 
+    'Mario, Kart, PC, jeu, course, jeu gratuit, multijoueur, éditeur de circuits';
 ?>
+
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="author" content="Timothé Malahieude" />
-<?php
-if ($hdescription) {
-	?>
-<meta name="description" content="<?php echo $hdescription; ?>" />
-	<?php
-}
-?>
-<meta name="keywords" content="<?php echo $language ? 'Mario, Kart, PC, game, race, free game, multiplayer, circuits editor':'Mario, Kart, PC, jeu, course, jeu gratuit, multijoueur, éditeur de circuits'; ?>" />
-<?php
-if ($hthumbnail) {
-	?>
-<meta name="thumbnail" content="<?php echo $hthumbnail; ?>" />
-<meta property="og:image" content="<?php echo $hthumbnail; ?>" />
-	<?php
-}
-?>
+
+<?php if ($hdescription): ?>
+    <meta name="description" content="<?= $hdescription ?>" />
+<?php endif; ?>
+
+<meta name="keywords" content="<?= $keywords ?>" />
+
+<?php if ($hthumbnail): ?>
+    <meta name="thumbnail" content="<?= $hthumbnail ?>" />
+    <meta property="og:image" content="<?= $hthumbnail ?>" />
+<?php endif; ?>
+
 <link rel="stylesheet" href="styles/main.css?reload=0" />
 <link rel="stylesheet" href="styles/creations.css" />
 <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
+
 <script async src="scripts/main.js"></script>
