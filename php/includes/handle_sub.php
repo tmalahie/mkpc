78ae62886ff9a1c420832b02035f2794e63138fb
Pianta <113540867+PiantaMK@users.noreply.github.com>
2025-04-13 11:50:19 +0200

    Rewrite handle_sub.php and add 8 char password requirement

--- php/includes/handle_sub.php
+++ php/includes/handle_sub.php
@@ -4,41 +4,45 @@ include('language.php');
 include('session.php');
 $inscrit = false;
 include('initdb.php');
-if (isset($_POST['pseudo']) && isset($_POST['code']) && isset($_POST['confirm']) && isset($_POST['email']) && isset($_POST['country'])) {
-	$pseudo = $_POST['pseudo'];
-	$code = $_POST['code'];
-	$confirm = $_POST['confirm'];
+
+if (isset($_POST['pseudo'], $_POST['code'], $_POST['confirm'], $_POST['email'], $_POST['country'])) {
+	$username = $_POST['pseudo'];
+	$password = $_POST['code'];
+	$password2 = $_POST['confirm'];
 	$email = $_POST['email'];
 	$country = $_POST['country'];
+	
 	include('utils-cooldown.php');
-	if (!$pseudo)
-		$message = $language ? 'Please enter a username':'Veuillez entrer un pseudo';
-	elseif (!$code)
-		$message = $language ? 'Please choose a password':'Veuillez choisir un mot de passe';
-	elseif ($code != $confirm)
-		$message = $language ? 'You made a mistake re-entering your password':'Vous avez fait une erreur en retapant votre mot de passe';
-	elseif (!preg_match('#^[a-zA-Z0-9_\-]+$#', $pseudo))
-		$message = $language ? 'You username mustn\'t contain special chars.<br />Allowed chars are : letters, numbers, the dash - and the underscore _':'Votre pseudo ne doit pas contenir de caract&egrave;res spéciaux.<br />Les caract&egrave;res autoris&eacute;s sont les lettres sans accents, les chiffres, le tiret - et le underscore _';
+	if (!$username)
+		$message =_('Please enter a username');
+	elseif (!$password)
+		$message = _('Please choose a password');
+	elseif ($password != $password2)
+		$message = _('You made a mistake re-entering your password');
+	elseif (strlen($password) < 8)
+		$message = _('Your password must be at least 8 characters long.');
+	elseif (!preg_match('#^[a-zA-Z0-9_\-]+$#', $username))
+		$message = _("You username mustn't contain special chars.<br />Allowed chars are : letters, numbers, the dash - and the underscore _");
 	elseif ($email && !preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#i", $email))
-		$message = ($language ? 'Please enter a valid email address':'Veuillez entrer une adresse email valide');
-	elseif ($getData=mysql_fetch_array(mysql_query('SELECT deleted FROM `mkjoueurs` WHERE nom="'.$pseudo.'"'))) {
+		$message = _('Please enter a valid email address');
+	elseif ($getData=mysql_fetch_array(mysql_query('SELECT deleted FROM `mkjoueurs` WHERE nom="'.$username.'"'))) {
 		if ($getData['deleted'])
-			$message = $language ? 'This account is already used and deleted. If you want to recover it, simply relog-in.':'Ce pseudo est déjà utilisé par un compte supprimé. S\'il s\'agit du vôtre et que vous souhaitez le récupérer, reconnectez-vous simplement avec vos identifiants.';
+			$message = _('This account is already used and deleted. If you want to recover it, simply relog-in.');
 		else
-			$message = $language ? 'This username is already used. Please choose another one.':'Ce pseudo est d&eacute;ja utilis&eacute;. Veuillez en choisir un autre.';
+			$message = _('This username is already used. Please choose another one.');
 	}
 	elseif (mysql_numrows(mysql_query('SELECT * FROM `ip_bans` WHERE ip1="'. $identifiants[0] .'" AND ip2="'. $identifiants[1] .'" AND ip3="'. $identifiants[2] .'" AND ip4="'. $identifiants[3] .'"')))
-		 $message = $language ? 'You have been banned.':'Vous avez &eacute;t&eacute; banni.';
+		 $message = _('You have been banned.');
 	elseif (isAccountCooldowned()) {
 		logCooldownEvent('profile');
-		$message = $language ? 'Too many accounts created recently, please try again later.':'Trop de comptes créés récemment, veuillez réessayer ultérieurement.';
+		$message = _('Too many accounts created recently, please try again later.');
 	}
 	else {
 		if ($getCountryId = mysql_fetch_array(mysql_query('SELECT id FROM mkcountries WHERE code="'. $country .'"')))
 			$countryId = $getCountryId['id'];
 		else
 			$countryId = 0;
-		mysql_query('INSERT INTO `mkjoueurs` VALUES (null, 0, "'.$pseudo.'", "'.password_hash($code,PASSWORD_DEFAULT).'", 0, 0, 0, 5000, 5000, 0, 2, 0,0)');
+		mysql_query('INSERT INTO `mkjoueurs` VALUES (null, 0, "'.$username.'", "'.password_hash($password,PASSWORD_DEFAULT).'", 0, 0, 0, 5000, 5000, 0, 2, 0,0)');
 		$id = mysql_insert_id();
 		if ($id) {
 			$_SESSION['mkid'] = $id;
@@ -47,13 +51,13 @@ if (isset($_POST['pseudo']) && isset($_POST['code']) && isset($_POST['confirm'])
 			$date = date('Y-m-d H:i:s');
 			mysql_query('INSERT INTO `mkprofiles` VALUES('. $id .', NULL,0,0,0,"","'.$_POST['pseudo'].'",0,"'.$email.'",'.$countryId.',NULL,"'.$date.'",NULL,"")');
 			include('setId.php');
-			mysql_query('UPDATE `mkrecords` SET player='.$id.' WHERE identifiant='.$identifiants[0].' AND identifiant2='.$identifiants[1].' AND identifiant3='.$identifiants[2].' AND identifiant4='.$identifiants[3].' AND (player=0 OR name="'.$pseudo.'")');
+			mysql_query('UPDATE `mkrecords` SET player='.$id.' WHERE identifiant='.$identifiants[0].' AND identifiant2='.$identifiants[1].' AND identifiant3='.$identifiants[2].' AND identifiant4='.$identifiants[3].' AND (player=0 OR name="'.$username.'")');
 			mysql_query('UPDATE `mkgametime` SET player='.$id.' WHERE identifiant='.$identifiants[0].' AND player=0');
 			mysql_query('UPDATE `mkgametimehist` SET player='.$id.' WHERE identifiant='.$identifiants[0].' AND player=0');
 			$inscrit = true;
 		}
 		else
-			$message = $language ? 'Sorry, an unknown error occured, please try again later.':'Désolé, une erreur inconnue est survenue, veuillez réessayer plus tard.';
+			$message = _('Sorry, an unknown error occured, please try again later.');
 	}
 }
 ?>
