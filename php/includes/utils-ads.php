0962194156912aa7b5d911754b27f2b09468bcab
Pianta <113540867+PiantaMK@users.noreply.github.com>
2025-04-13 11:51:23 +0200

    Disable ads outside of prod

--- php/includes/utils-ads.php
+++ php/includes/utils-ads.php
@@ -1,4 +1,27 @@
 <?php
+function makeAd($width, $height, $adSlot) {
+    if (!str_contains($_SERVER['HTTP_HOST'], 'malahieude.net')) {
+        return; // no ads on selfhost
+    }
+    ?>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <ins class="adsbygoogle"
+        style="display:inline-block;width:<?= $width ?>px;height:<?= $height ?>px"
+        data-ad-client="ca-pub-1340724283777764"
+        data-ad-slot="<?= $adSlot ?>"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    <?php
+    require_once('grlp.php');
+}
+function showRegularAd() {
+    makeAd(728, 90, '4919860724');
+}
+
+function showSmallAd() {
+    makeAd(468, 60, '6691323567');
+}
 function showRegularAdSection() {
     ?><p class="pub"><?php
     showRegularAd();
@@ -13,28 +36,4 @@ function showGameAdSection() {
     ?><div id="vPub"><?php
     showSmallAd();
     ?></div><?php
-}
-function showRegularAd() {
-    ?><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- Forum MKPC -->
-    <ins class="adsbygoogle"
-        style="display:inline-block;width:728px;height:90px"
-        data-ad-client="ca-pub-1340724283777764"
-        data-ad-slot="4919860724"></ins>
-    <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-    </script><?php
-    require_once('grlp.php');
-}
-function showSmallAd() {
-    ?><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- Mario Kart PC -->
-    <ins class="adsbygoogle"
-        style="display:inline-block;width:468px;height:60px"
-        data-ad-client="ca-pub-1340724283777764"
-        data-ad-slot="6691323567"></ins>
-    <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-    </script><?php
-    require_once('grlp.php');
 }
