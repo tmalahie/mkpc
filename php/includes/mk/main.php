8fb045b5c9b59b237ccac26d64d5e456e2ea612a
tmalahie <t.malahieude@gmail.com>
2025-05-07 20:39:58 +0200

    v281

--- php/includes/mk/main.php
+++ php/includes/mk/main.php
@@ -2,5 +2,5 @@
 if ($_SERVER['HTTP_HOST'] !== 'mkpc.malahieude.net')
 	echo '<script type="text/javascript" src="scripts/mk.js"></script>';
 else
-	echo '<script type="text/javascript" src="scripts/mk.v280.js"></script>';
+	echo '<script type="text/javascript" src="scripts/mk.v281.js"></script>';
 ?>
