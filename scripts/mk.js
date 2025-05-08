1bcf7e8a565dd6a398ddfd93816377a829ee5f0c
Anthcny144 <anthonysimonemailpro@gmail.com>
2025-05-08 19:40:00 +0200

    Increase red shell speed & decrease blue shell speed
    Slightly nerfed the blue & dark shell speed, thet now have a global 33% speed buff
    
    Removed red shells' speed boost from jumps as this is now useless

--- scripts/mk.js
+++ scripts/mk.js
@@ -7900,7 +7900,7 @@ var itemBehaviors = {
 
 			var steps = 5;
 			for (var l=0;l<steps;l++) {
-				var dSpeed = (fSprite.heightinc ? 15:12)*cappedRelSpeed()/steps;
+				var dSpeed = 14*cappedRelSpeed()/steps;
 				var fTeleport;
 				if (fSprite.owner != -1) {
 					if (fSprite.cannon) {
@@ -8213,7 +8213,7 @@ var itemBehaviors = {
 						var fMove2 = fMoveX*fMoveX + fMoveY*fMoveY;
 						var itemBehavior = itemBehaviors["carapace-bleue"];
 						if (fSprite.cooldown == itemBehavior.cooldown0) {
-							var dSpeed = 12*relSpeed * spdMult;
+							var dSpeed = 10*relSpeed * spdMult;
 							if (fMove2 > dSpeed*dSpeed) {
 								var fNewMove = Math.sqrt(fMove2)/dSpeed;
 								fMoveX /= fNewMove;
@@ -8413,7 +8413,7 @@ var itemBehaviors = {
 						var fMove2 = fMoveX*fMoveX + fMoveY*fMoveY;
 						var itemBehavior = itemBehaviors["carapace-noire"];
 						if (fSprite.cooldown == itemBehavior.cooldown0) {
-							var dSpeed = 12*relSpeed * spdMult;
+							var dSpeed = 10*relSpeed * spdMult;
 							if (fMove2 > dSpeed*dSpeed) {
 								var fNewMove = Math.sqrt(fMove2)/dSpeed;
 								fMoveX /= fNewMove;
