tmalahie/mkpc

commit 8d0e4e360c37608f8278724f6b7a8150915d6577

Fix box decor issues
Fixed damagingItems not taken in account if player is invincible

Fixed items not dropping when decor is destroyed by an item / invincible player

scripts/mk.js
@@ -7275,7 +7275,7 @@ var itemBehaviors = {
 			}
 
 			if (!fSprite.sprites) {
-				// don't remove pow if previous pow is in its wait after despawn animation
+				// don't remove pow if previous pow is in its waiting animation
 				const shouldDelete = (items.pow.length > 1 && itemDistribution.powx2 != 1) ||
 									 (items.pow.length === 2 && items.pow[0].countstate < 9);
 
@@ -7779,7 +7779,7 @@ var itemBehaviors = {
 					detruit(fSprite,true);
 					break;
 				}
-				else if (!isMoving || canMoveTo(fSprite.x,fSprite.y,fSprite.z, fMoveX,fMoveY)) {
+				else if (!isMoving || canMoveTo(fSprite.x,fSprite.y,fSprite.z, fMoveX,fMoveY, null, null, fSprite)) {
 					if (ctx && ctx.checkCollisions) {
 						ctx.checkCollisions(fSprite);
 						if (fSprite.deleted)
@@ -8159,7 +8159,7 @@ var itemBehaviors = {
 				collisionItem = fSprite;
 				collisionFloor = null;
 				collisionLap = fSprite.ailap;
-				if (((fSprite.owner == -1) || fTeleport || ((fSprite.z || !tombe(fNewPosX, fNewPosY)) && canMoveTo(fSprite.x,fSprite.y,fSprite.z, fMoveX,fMoveY))) && !touche_banane(fNewPosX, fNewPosY, oSpriteExcept) && !touche_banane(fSprite.x, fSprite.y, oSpriteExcept) && !touche_crouge(fNewPosX, fNewPosY, fSpriteExcept) && !touche_crouge(fSprite.x, fSprite.y, fSpriteExcept) && !touche_cverte(fNewPosX, fNewPosY, oSpriteExcept) && !touche_cverte(fSprite.x, fSprite.y, oSpriteExcept) && !touche_bobomb(fNewPosX, fNewPosY, oSpriteExcept, {transparent:true}) && !touche_bobomb(fSprite.x, fSprite.y, oSpriteExcept, {transparent:true})) {
+				if (((fSprite.owner == -1) || fTeleport || ((fSprite.z || !tombe(fNewPosX, fNewPosY)) && canMoveTo(fSprite.x,fSprite.y,fSprite.z, fMoveX,fMoveY, null, null, fSprite))) && !touche_banane(fNewPosX, fNewPosY, oSpriteExcept) && !touche_banane(fSprite.x, fSprite.y, oSpriteExcept) && !touche_crouge(fNewPosX, fNewPosY, fSpriteExcept) && !touche_crouge(fSprite.x, fSprite.y, fSpriteExcept) && !touche_cverte(fNewPosX, fNewPosY, oSpriteExcept) && !touche_cverte(fSprite.x, fSprite.y, oSpriteExcept) && !touche_bobomb(fNewPosX, fNewPosY, oSpriteExcept, {transparent:true}) && !touche_bobomb(fSprite.x, fSprite.y, oSpriteExcept, {transparent:true})) {
 					var aPos = [fSprite.x,fSprite.y];
 					fSprite.x = fNewPosX;
 					fSprite.y = fNewPosY;
@@ -11904,11 +11904,15 @@ function pointCrossPolygon(iX,iY,nX,nY, oPoints) {
 }
 
 var jumpHeight0 = 1.175, jumpHeight1 = jumpHeight0+1e-5;
-function canMoveTo(iX,iY,iZ, iI,iJ, iP, iZ0) {
+function canMoveTo(iX,iY,iZ, iI,iJ, iP, iZ0, obj) {
 	var nX = iX+iI, nY = iY+iJ;
 	var lMap = getCurrentLMap(collisionLap);
 
 	if (lMap.decor) {
+		const fromSelf = (obj == oPlayers[0] || obj.owner == oPlayers[0].id);
+		const clientSideDrop = !isOnline || (fromSelf && !onlineSpectatorId);
+		const dropPos = {x: nX + iI*2.5, y: nY+iJ*2.5};
+
 		for (var type in lMap.decor) {
 			var decorBehavior = decorBehaviors[type];
 			var hitboxSize = decorBehavior.hitbox||DEFAULT_DECOR_HITBOX;
@@ -11924,30 +11928,32 @@ function canMoveTo(iX,iY,iZ, iI,iJ, iP, iZ0) {
 							if (decorBehavior.breaking || isBreakingItem(decorBehavior)) {
 								if (collisionPlayer.speed > 4) {
 									handleDecorHit(i,type, lMap);
+									if (clientSideDrop)
+										dropBoxDecorLoot(obj, dropPos);
 									if (collisionPlayer.turbodrift)
 										collisionPlayer.turbodrift = 0;
-									if (decorBehavior.bonus) {
-										if (!isOnline || (collisionPlayer == oPlayers[0] && !onlineSpectatorId)) {
-											var bonusType = "champi";
-											if (course != "CM" && Math.random() < 0.5)
-												bonusType = "banane";
-											addNewItem(collisionPlayer, {type: bonusType, team:collisionPlayer.team, x:(nX+iI*2.5),y:(nY+iJ*2.5), z:0});
-										}
-									}
 								}
 							}
 						}
 						else if (collisionTest == COL_OBJ && collisionItem && decorBehavior.damagingItems) {
-							if (decorBehavior.damagingItems[collisionItem.type])
+							if (decorBehavior.damagingItems[collisionItem.type]) {
 								handleDecorHit(i,type, lMap);
+								if (clientSideDrop)
+									dropBoxDecorLoot(obj, dropPos);
+							}
 						}
 						if (decorBehavior.transparent)
 							continue;
 						return false;
 					}
 					else {
-						handleDecorHit(i,type, lMap);
-						break;
+						if ((iP && isBreakingItem(decorBehavior)) || decorBehavior.breaking) {
+							handleDecorHit(i,type, lMap);
+							if (clientSideDrop)
+								dropBoxDecorLoot(obj, dropPos);
+							break;
+						}
+						return false;
 					}
 				}
 			}
@@ -15382,6 +15388,47 @@ function playPow(i, oKart, oKartOwner, fSprite) {
     }
 }
 
+function dropBoxDecorLoot(obj, pos, distrib) {
+	// default drop distribution
+	if (!distrib)
+		distrib = course == "CM" ? {"champi": 1} : {"champi": 0.5, "banane": 0.5};
+
+	// get total probability
+	let total = 0;
+	for (const item in distrib)
+		total += distrib[item];
+
+	// pick random item in distrib
+	const rng = Math.random() * total;
+	let sum = 0;
+	let item;
+	
+	for (const itemType in distrib) {
+		sum += distrib[itemType];
+		if (rng < sum) {
+			// if obj is not a player, find it
+			if (!obj.personnage) {
+				obj = aKarts.find(oKart => oKart.id === obj.owner);
+			}
+
+			item = {
+				type: itemType,
+				owner: obj.id,
+				team: obj.team,
+				x: pos.x,
+				y: pos.y,
+				z: 0
+			};
+
+			if (itemType === "bobomb")
+				item.cooldown = 30;
+
+			dropNewItem(obj, item);
+			return;
+		}
+	}
+}
+
 function touche_asset(aPosX,aPosY,aPosZ, iX,iY) {
 	var lMap = getCurrentLMap(collisionLap);
 	var turningAssets = ["pointers", "flippers"];
@@ -17057,7 +17104,7 @@ function move(getId, triggered) {
 	collisionItem = null;
 	var kartReplaced = false;
 	var nPosZ0;
-	if (oKart.cannon || canMoveTo(aPosX,aPosY,oKart.z, fMoveX,fMoveY, oKart.protect, oKart.z0||0) || oKart.billball) {
+	if (oKart.cannon || canMoveTo(aPosX,aPosY,oKart.z, fMoveX,fMoveY, oKart.protect, oKart.z0||0, oKart) || oKart.billball) {
 		oKart.x = fNewPosX;
 		oKart.y = fNewPosY;
 		if (oKart.cpu)
@@ -17094,7 +17141,7 @@ function move(getId, triggered) {
 		for (var i=5;i>0;i--) {
 			oKart.x += horizontality[0]*s*i/5;
 			oKart.y += horizontality[1]*s*i/5;
-			if (canMoveTo(aPosX,aPosY,oKart.z, oKart.x-aPosX,oKart.y-aPosY, oKart.protect, oKart.z0||0))
+			if (canMoveTo(aPosX,aPosY,oKart.z, oKart.x-aPosX,oKart.y-aPosY, oKart.protect, oKart.z0||0, oKart))
 				break;
 			else {
 				oKart.x = aPosX;

commit 02c00879f76ea585e97390fb151e55db0aa07f9f

Fixed breaking decor behaviors
Fixed decors not breaking issues introduced by commit 8d0e4e3

Removed the speed check when breaking decors while player is invincible

Removed the MT reset when breaking transparent decors

scripts/mk.js
@@ -7779,7 +7779,7 @@ var itemBehaviors = {
 					detruit(fSprite,true);
 					break;
 				}
-				else if (!isMoving || canMoveTo(fSprite.x,fSprite.y,fSprite.z, fMoveX,fMoveY, null, null, fSprite)) {
+				else if (!isMoving || canMoveTo(fSprite.x,fSprite.y,fSprite.z, fMoveX,fMoveY)) {
 					if (ctx && ctx.checkCollisions) {
 						ctx.checkCollisions(fSprite);
 						if (fSprite.deleted)
@@ -8159,7 +8159,7 @@ var itemBehaviors = {
 				collisionItem = fSprite;
 				collisionFloor = null;
 				collisionLap = fSprite.ailap;
-				if (((fSprite.owner == -1) || fTeleport || ((fSprite.z || !tombe(fNewPosX, fNewPosY)) && canMoveTo(fSprite.x,fSprite.y,fSprite.z, fMoveX,fMoveY, null, null, fSprite))) && !touche_banane(fNewPosX, fNewPosY, oSpriteExcept) && !touche_banane(fSprite.x, fSprite.y, oSpriteExcept) && !touche_crouge(fNewPosX, fNewPosY, fSpriteExcept) && !touche_crouge(fSprite.x, fSprite.y, fSpriteExcept) && !touche_cverte(fNewPosX, fNewPosY, oSpriteExcept) && !touche_cverte(fSprite.x, fSprite.y, oSpriteExcept) && !touche_bobomb(fNewPosX, fNewPosY, oSpriteExcept, {transparent:true}) && !touche_bobomb(fSprite.x, fSprite.y, oSpriteExcept, {transparent:true})) {
+				if (((fSprite.owner == -1) || fTeleport || ((fSprite.z || !tombe(fNewPosX, fNewPosY)) && canMoveTo(fSprite.x,fSprite.y,fSprite.z, fMoveX,fMoveY))) && !touche_banane(fNewPosX, fNewPosY, oSpriteExcept) && !touche_banane(fSprite.x, fSprite.y, oSpriteExcept) && !touche_crouge(fNewPosX, fNewPosY, fSpriteExcept) && !touche_crouge(fSprite.x, fSprite.y, fSpriteExcept) && !touche_cverte(fNewPosX, fNewPosY, oSpriteExcept) && !touche_cverte(fSprite.x, fSprite.y, oSpriteExcept) && !touche_bobomb(fNewPosX, fNewPosY, oSpriteExcept, {transparent:true}) && !touche_bobomb(fSprite.x, fSprite.y, oSpriteExcept, {transparent:true})) {
 					var aPos = [fSprite.x,fSprite.y];
 					fSprite.x = fNewPosX;
 					fSprite.y = fNewPosY;
@@ -11904,15 +11904,11 @@ function pointCrossPolygon(iX,iY,nX,nY, oPoints) {
 }
 
 var jumpHeight0 = 1.175, jumpHeight1 = jumpHeight0+1e-5;
-function canMoveTo(iX,iY,iZ, iI,iJ, iP, iZ0, obj) {
+function canMoveTo(iX,iY,iZ, iI,iJ, iP, iZ0) {
 	var nX = iX+iI, nY = iY+iJ;
 	var lMap = getCurrentLMap(collisionLap);
 
 	if (lMap.decor) {
-		const fromSelf = (obj == oPlayers[0] || obj.owner == oPlayers[0].id);
-		const clientSideDrop = !isOnline || (fromSelf && !onlineSpectatorId);
-		const dropPos = {x: nX + iI*2.5, y: nY+iJ*2.5};
-
 		for (var type in lMap.decor) {
 			var decorBehavior = decorBehaviors[type];
 			var hitboxSize = decorBehavior.hitbox||DEFAULT_DECOR_HITBOX;
@@ -11922,39 +11918,54 @@ function canMoveTo(iX,iY,iZ, iI,iJ, iP, iZ0, obj) {
 				if (nX > oBox[0]-hitboxSize && nX < oBox[0]+hitboxSize && nY > oBox[1]-hitboxSize && nY < oBox[1]+hitboxSize && (Math.abs((oBox[3]?oBox[3]:0)-iZ)<hitboxHeight)) {
 					if ((oBox[3] == undefined) && (iX > oBox[0]-hitboxSize) && (iX < oBox[0]+hitboxSize) && (iY > oBox[1]-hitboxSize) && (iY < oBox[1]+hitboxSize))
 						continue;
-					if (!iP || decorBehavior.unbreaking) {
-						collisionDecor = type;
-						if (collisionTest == COL_KART) {
-							if (decorBehavior.breaking || isBreakingItem(decorBehavior)) {
-								if (collisionPlayer.speed > 4) {
-									handleDecorHit(i,type, lMap);
-									if (clientSideDrop)
-										dropBoxDecorLoot(obj, dropPos);
-									if (collisionPlayer.turbodrift)
-										collisionPlayer.turbodrift = 0;
-								}
-							}
-						}
-						else if (collisionTest == COL_OBJ && collisionItem && decorBehavior.damagingItems) {
-							if (decorBehavior.damagingItems[collisionItem.type]) {
-								handleDecorHit(i,type, lMap);
-								if (clientSideDrop)
-									dropBoxDecorLoot(obj, dropPos);
-							}
-						}
-						if (decorBehavior.transparent)
-							continue;
+
+					if (decorBehavior.unbreaking)
 						return false;
-					}
-					else {
-						if ((iP && isBreakingItem(decorBehavior)) || decorBehavior.breaking) {
+
+					let fromSelf;
+					if (collisionTest === COL_KART && collisionPlayer === oPlayers[0])
+						fromSelf = true;
+					else if (collisionTest === COL_OBJ && collisionItem.owner === oPlayers[0].id)
+						fromSelf = true;
+					
+					const collisionFrom = collisionTest === COL_KART ? collisionPlayer : collisionItem;
+					const clientSideDrop = !isOnline || (fromSelf && !onlineSpectatorId);
+					const dropPos = {x: nX + iI*2.5, y: nY+iJ*2.5};
+
+					collisionDecor = type;
+
+					// player collision
+					if (collisionTest == COL_KART) {
+						const canBreak = decorBehavior.breaking ||
+						(iP && isBreakingItem(decorBehavior) ||
+						(!iP && collisionPlayer.champi && isBreakingItem(decorBehavior)));
+
+						if (!canBreak)
+							return false;
+
+						if (collisionPlayer.speed > 4 || iP) {
 							handleDecorHit(i,type, lMap);
-							if (clientSideDrop)
-								dropBoxDecorLoot(obj, dropPos);
-							break;
+							if (decorBehavior.bonus && clientSideDrop)
+								dropBoxDecorLoot(collisionFrom, dropPos);
+							if (collisionPlayer.turbodrift && !decorBehavior.transparent)
+								collisionPlayer.turbodrift = 0;
 						}
-						return false;
 					}
+
+					// item collision
+					else if (collisionTest == COL_OBJ && collisionItem && decorBehavior.damagingItems) {
+						if (decorBehavior.damagingItems[collisionItem.type]) {
+							handleDecorHit(i,type, lMap);
+							if (decorBehavior.bonus && clientSideDrop)
+								dropBoxDecorLoot(collisionFrom, dropPos);
+						}
+					}
+
+					if (decorBehavior.transparent)
+						continue;
+
+					if (!iP)
+						return false;
 				}
 			}
 		}
@@ -12043,7 +12054,7 @@ function getLineHorizontality(iX,iY, nX,nY, lines) {
 	return res;
 }
 function isBreakingItem(decorBehavior) {
-	if (!decorBehavior.damagingItems) return false;
+	if (!decorBehavior.damagingItems) return true;
 	if (decorBehavior.damagingItems.megachampi && collisionPlayer.megachampi) return true;
 	if (decorBehavior.damagingItems.billball && collisionPlayer.billball) return true;
 	if (decorBehavior.damagingItems.etoile && collisionPlayer.etoile) return true;
@@ -17105,7 +17116,7 @@ function move(getId, triggered) {
 	collisionItem = null;
 	var kartReplaced = false;
 	var nPosZ0;
-	if (oKart.cannon || canMoveTo(aPosX,aPosY,oKart.z, fMoveX,fMoveY, oKart.protect, oKart.z0||0, oKart) || oKart.billball) {
+	if (oKart.cannon || canMoveTo(aPosX,aPosY,oKart.z, fMoveX,fMoveY, oKart.protect, oKart.z0||0) || oKart.billball) {
 		oKart.x = fNewPosX;
 		oKart.y = fNewPosY;
 		if (oKart.cpu)
@@ -17142,7 +17153,7 @@ function move(getId, triggered) {
 		for (var i=5;i>0;i--) {
 			oKart.x += horizontality[0]*s*i/5;
 			oKart.y += horizontality[1]*s*i/5;
-			if (canMoveTo(aPosX,aPosY,oKart.z, oKart.x-aPosX,oKart.y-aPosY, oKart.protect, oKart.z0||0, oKart))
+			if (canMoveTo(aPosX,aPosY,oKart.z, oKart.x-aPosX,oKart.y-aPosY, oKart.protect, oKart.z0||0))
 				break;
 			else {
 				oKart.x = aPosX;
@@ -18461,6 +18472,10 @@ function handleDecorExplosions(fSprite, callback) {
 		if (!pMap.decor) return;
 		for (var type in lMap.decor) {
 			var decorBehavior = decorBehaviors[type];
+
+			if (decorBehavior.unbreaking)
+				return false;
+
 			if (decorBehavior.damagingItems && decorBehavior.damagingItems[fSprite.type]) {
 				for (var i=0;i<lMap.decor[type].length;i++) {
 					var oBox = lMap.decor[type][i];