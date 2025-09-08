a155da442479e01ef5f057c4bf8d1d63a16f1886
tmalahie <t.malahieude@gmail.com>
2025-09-08 21:22:18 +0200

    Prevent to take a rail while in cannon

--- scripts/mk.js
+++ scripts/mk.js
@@ -18579,7 +18579,7 @@ function move(getId, triggered) {
 			}
 		}
 	}
-	if (!oKart.rail && !oKart.teleport && !oKart.billball && !(oKart.z > 0 && oKart.heightinc >= 0))
+	if (!oKart.rail && !oKart.teleport && !oKart.billball && !oKart.cannon && !(oKart.z > 0 && oKart.heightinc >= 0))
 		checkRailEnter(getId, aPosX,aPosY,aPosZ);
 	oKart.sliding = undefined;
 	if (!oKart.z) {
