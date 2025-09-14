43e23d7f5db57fdf8aa29f248011e24082c8eaf0
Pianta <113540867+PiantaMK@users.noreply.github.com>
2025-09-14 12:05:06 +0200

    dont cache mk.js on localhost unless ?nocache

--- php/includes/mk/main.php
+++ php/includes/mk/main.php
@@ -1,6 +1,12 @@
 <?php
-if ($_SERVER['HTTP_HOST'] !== 'mkpc.malahieude.net')
-	echo '<script type="text/javascript" src="scripts/mk.js"></script>';
-else
-	echo '<script type="text/javascript" src="scripts/mk.v301.js"></script>';
+if ($_SERVER['HTTP_HOST'] !== 'mkpc.malahieude.net') {
+	// cache busting, can be disable with ?nocache/&nocache
+	$cb = '?cb='.time();
+	if (isset($_GET['nocache'])) {
+		$cb = '';
+	}
+	echo '<script type="text/javascript" src="scripts/mk.js'.$cb.'"></script>';
+} else {
+	echo '<script type="text/javascript" src="scripts/mk.v297.js"></script>';
+}
 ?>
